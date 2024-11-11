3d05dfc06166eb7ba5723ada67e90d349a405cde
GooSeMaNVk <vista.vk@gmail.com>
2024-11-11 19:54:05 +0700

    Hello

--- tests/test_basket_discount.py
+++ tests/test_basket_discount.py
@@ -1,52 +1,33 @@
-from playwright.sync_api import sync_playwright
-import time as t
-from pages.basket_page import BasketPage
-from pages.product_page import ProductPage
+import pytest
 
+def test_apply_promo_code(product_page, basket_page):
+    # Получение названия товара "Происхождение"
+    product_name = product_page.get_product_origin()
+    print(f"Добавленный товар: {product_name}")
 
-def test_apply_promo_code():
-    with sync_playwright() as p:
-        # Инициализация браузера и открытие новой страницы
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
+    # Добавление товара в корзину
+    product_page.add_product_to_basket()
 
-        # Переход на сайт
-        page.goto("https://basket.genotek.ru")
+    # Переход на страницу корзины
+    basket_page.page.goto("https://basket.genotek.ru/basket")
 
-        # Создаем экземпляры страниц
-        product_page = ProductPage(page)
-        basket_page = BasketPage(page)
+    # Получение текущей цены товара до применения промокода
+    try:
+        price_before = basket_page.get_product_price(after_promo=False)  # Получаем цену до промокода
+        print(f"Цена товара до применения промокода: {price_before}")
+    except Exception as e:
+        print(f"Ошибка при получении цены до промокода: {e}")
 
-        # Получение названия товара "Происхождение"
-        product_name = product_page.get_product_origin()
-        print(f"Добавленный товар: {product_name}")
+    # Применение промокода
+    basket_page.apply_promo_code("genotek5")
 
-        # Добавление товара в корзину
-        product_page.add_product_to_basket()
+    # Ждем обновления цены товара
+    try:
+        # Ожидаем, что обновленная цена будет видна
+        basket_page.page.wait_for_selector("overlay-scrollbars", timeout=10000)
 
-        # Переход на страницу корзины
-        page.goto("https://basket.genotek.ru/basket")
-
-        # Получение текущей цены товара до применения промокода
-        try:
-            price_before = basket_page.get_product_price(after_promo=False)  # Получаем цену до промокода
-            print(f"Цена товара до применения промокода: {price_before}")
-        except Exception as e:
-            print(f"Ошибка при получении цены до промокода: {e}")
-
-        # Применение промокода
-        basket_page.apply_promo_code("genotek5")
-
-        # Ждем обновления цены товара
-        try:
-            # Ожидаем, что обновленная цена будет видна
-            page.wait_for_selector("overlay-scrollbars", timeout=10000)
-
-            # Получаем обновленную цену после применения промокода
-            price_after = basket_page.get_product_price(after_promo=True)  # Получаем цену после промокода
-            print(f"Цена товара после применения промокода: {price_after}")
-        except Exception as e:
-            print(f"Не удалось обновить цену товара после применения промокода: {e}")
-
-        # Закрытие браузера
-        browser.close()
+        # Получаем обновленную цену после применения промокода
+        price_after = basket_page.get_product_price(after_promo=True)  # Получаем цену после промокода
+        print(f"Цена товара после применения промокода: {price_after}")
+    except Exception as e:
+        print(f"Не удалось обновить цену товара после применения промокода: {e}")
